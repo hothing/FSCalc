e135c05148e028bf67d90ee56b30503c77060840
Mykhayl Puzanov <barbos@inbox.ru>
2020-12-05 12:45:53 +0300

    A new Expression 'sequence' has been added; a new Expression 'setVar' has been added, but does not work

--- src/App/Program.fs
+++ src/App/Program.fs
@@ -9,23 +9,28 @@ let main argv =
     let environment = Map.ofList [ "a", 1 ; "b", 2 ; "c", 3 ]
     // Create an expression tree that represents
     // the expression: a + 2 * b.
-    let expr1 = Add(Variable "a", Multiply(Number 2, Variable "b"))
+    let expr1 = Add(GetVar "a", Multiply(Number 2, GetVar "b"))
     // Evaluate the expression #1, given the
     // table of values for the variables.
     let result = Evaluate environment expr1
     printfn "Result #1: %d" result
 
-    let expr2 = Sub(Variable "a", Divide(Number 10, Variable "b"))
+    let expr2 = Sub(GetVar "a", Divide(Number 10, GetVar "b"))
     let result = Evaluate environment expr2
     printfn "Result #2: %d" result
 
-    let expr3 = Neg(Sub(Variable "a", Divide(Number 10, If(Number 1, Variable "b", Number 0))))
+    let expr3 = Neg(Sub(GetVar "a", Divide(Number 10, If(Number 1, GetVar "b", Number 0))))
     let result = Evaluate environment expr3
     printfn "Result #3: %d" result
 
-    let expr4 = Neg(Sub(Variable "a", Divide(Number 10, If(Number 0, Variable "b", Number 0))))
-    let result = Evaluate environment expr4
+    let expr4 = SetVar("c" , Neg(Sub(GetVar "a", Divide(Number 10, If(Number 1, GetVar "b", Number 0)))))
+    let result = Evaluate environment (GetVar "c")
     printfn "Result #4: %d" result
+
+    let lexp = [Add(GetVar "a", Multiply(Number 2, GetVar "b")); If(Number 1, GetVar "b", Number 0)]
+    let expr5 = Seq(lexp)
+    let result = Evaluate environment expr5
+    printfn "Result #5: %d" result
 
     0 // return an integer exit code
 
--- src/Library/Library.fs
+++ src/Library/Library.fs
@@ -1,23 +1,31 @@
 module Library
 
+//open List
+
 type Expression =
     | Number of int
-    | Variable of string
+    | GetVar of string
+    | SetVar of string * Expression 
     | Add of Expression * Expression
     | Sub of Expression * Expression
     | Multiply of Expression * Expression
     | Divide of Expression * Expression
     | Neg of Expression
     | If of Expression * Expression * Expression
+    | Seq of Expression list
 
 let rec Evaluate (env:Map<string,int>) exp =
     match exp with
     | Number n -> n
-    | Variable id -> env.[id]
+    | GetVar id -> env.[id]
+    | SetVar (id, value) -> let res = Evaluate env value
+                            let nenv = env.Add(id, res) //BUG: it does not work
+                            res
     | Add (x, y) -> Evaluate env x + Evaluate env y
     | Sub (x, y) -> Evaluate env x - Evaluate env y
     | Multiply (x, y) -> Evaluate env x * Evaluate env y
     | Divide (x, y) -> Evaluate env x / Evaluate env y
     | Neg (x) -> - Evaluate env x
-    | If (cond, eTrue, eFalse) -> if (Evaluate env cond) = 0 then (Evaluate env eFalse) else (Evaluate env eTrue) 
+    | If (cond, eTrue, eFalse) -> if (Evaluate env cond) = 0 then (Evaluate env eFalse) else (Evaluate env eTrue)
+    | Seq list -> List.fold (fun a x -> Evaluate env x) 0 list 
     
